7c2827955650254035d99be2762e113f0a0a6449
twanfan <tfancy@126.com>
2021-01-19 17:28:59 +0800

    变更：增加版本表格导航

---
+++ src/router/modules/version.js
@@ -0,0 +1,29 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+import Layout from '@/layout'
+
+const versionRouter = {
+  path: '/version',
+  component: Layout,
+  redirect: '/version/complex-table',
+  name: 'Version',
+  meta: {
+    title: 'TCL Versions',
+    icon: 'table'
+  },
+  children: [
+    {
+      path: 'detail-table',
+      component: () => import('@/views/version/detail-table'),
+      name: 'DetailTable',
+      meta: { title: 'Detail Table' }
+    },
+    {
+      path: 'analysis-table',
+      component: () => import('@/views/version/analysis-table'),
+      name: 'AnalysisTable',
+      meta: { title: 'Analysis Table' }
+    }
+  ]
+}
+export default versionRouter
